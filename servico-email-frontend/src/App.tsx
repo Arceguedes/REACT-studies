2902850420e757c847724732e1dba14038279d9f
Julia Guedes <guedesarce@gmail.com>
2024-02-11 11:04:37 -0300

    Insertion of Dribbble styles using Chakra components and box-shadows from CSS Scan

--- servico-email-frontend/src/App.tsx
+++ servico-email-frontend/src/App.tsx
@@ -3,18 +3,44 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import PrimaryInput from './components/input/PrimaryInput'
+import { Button, Spacer } from '@chakra-ui/react'
 
 function App() {
   const [email, setEmail] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [secondName, setSecondName] = useState("")
+
 
   return(
   <div className='container'>
+    <form>
+    <div className='name-form-container'>
+      <PrimaryInput
+        value={firstName}
+        onChange={event => setFirstName(event.target.value)} 
+        name="firstName"
+        label="First Name"
+        placeholder='Joana'
+        />
+      <PrimaryInput
+        value={secondName}
+        onChange={event => setSecondName(event.target.value)} 
+        name="SecondName"
+        label="Second Name"
+        placeholder='Dark'
+        />
+      </div>
+    <Spacer height="4"/>
     <PrimaryInput
       value={email}
       onChange={event => setEmail(event.target.value)} 
       name="email"
-      label="Digite seu Email "
+      label="Email"
+      placeholder='joanadark@react.com'
       />
+    <Spacer height="4"/>
+    <Button colorScheme='green' width="100%" marginTop="4">Enviar</Button>
+    </form>
   </div>
   )
 }
--- servico-email-frontend/src/components/input/PrimaryInput.tsx
+++ servico-email-frontend/src/components/input/PrimaryInput.tsx
@@ -7,18 +7,20 @@ interface PrimaryInputProps{
     name: string,
     value: string,
     onChange: ChangeEventHandler<HTMLInputElement>,
-    label: string
+    label: string,
+    placeholder: string
 }
 
 
-export default function PrimaryInput({name, value, onChange, label} : PrimaryInputProps){
+export default function PrimaryInput({name, value, onChange, label, placeholder} : PrimaryInputProps){
     return(
         <div className="input-container">
         <label className="label">{label}</label>
         <Input 
             variant='filled' 
-            placeholder='Filled' 
+            placeholder={placeholder} 
             name={name} 
+            value={value}
             onChange={onChange}
         />
         </div>
